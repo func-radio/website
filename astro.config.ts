50ba131dca0feb4e0b956aae2e1a1a246b2d3b86
Michael Thomas <michaelhthomas@outlook.com>
2025-11-13 18:55:01 -0500

    chore: add preact

--- astro.config.ts
+++ astro.config.ts
@@ -4,11 +4,13 @@ import tailwindcss from "@tailwindcss/vite";
 
 import icon from "astro-icon";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-	site: "https://funcradio.org",
-	vite: {
-		plugins: [tailwindcss()],
-	},
-	integrations: [icon()],
+    site: "https://funcradio.org",
+    vite: {
+        plugins: [tailwindcss()],
+    },
+    integrations: [icon(), preact()],
 });
