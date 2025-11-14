7380dde50930b7ced9c581ce90e59377b0f282c8
Michael Thomas <michaelhthomas@outlook.com>
2025-11-13 22:17:32 -0500

    chore: deps & format

--- astro.config.ts
+++ astro.config.ts
@@ -8,9 +8,9 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://funcradio.org",
-    vite: {
-        plugins: [tailwindcss()],
-    },
-    integrations: [icon(), preact()],
+	site: "https://funcradio.org",
+	vite: {
+		plugins: [tailwindcss()],
+	},
+	integrations: [icon(), preact()],
 });
